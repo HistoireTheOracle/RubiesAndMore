cced4e8be7fb1503d85907cb45d443614afed027
HistoireTheOracle <peter5th1234@gmail.com>
2021-07-07 12:55:26 -0500

    Recreating the mod from scratch using actual forge code. Changing IDE to Visual Studio Code.

---
+++ src/main/java/net/mcreator/rubiesandmore/GemFinatic.java
@@ -0,0 +1,14 @@
+package net.mcreator.rubiesandmore.Inits;
+
+import net.mcreator.rubiesandmore;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import net.minecraftforge.registries.DeferredRegister;
+
+import net.minecraft.village.PointOfInterestType;
+import net.minecraft.entity.merchant.villager.VillagerProfession;
+import net.minecraft.village.PointOfInterestType;
+
+public class VillagerInit {
+	public static final DeferredRegister<PointOfInterestType> POINT_OF_INTEREST_TYPES = DeferredRegister.create(ForgeRegistries.POI_TYPES,)	
+}
